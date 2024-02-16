3b1c89dd480799acb4b4f3ca2ee031ab8494b1c5
musa17-hawa <musa17-hawa@github.com>
2024-02-16 15:07:14 -0800

    Exercise 3, Polynoms representation using structs

--- projects/exerciseThree/Polynom.c
+++ projects/exerciseThree/Polynom.c
@@ -3,43 +3,107 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+void insertionSort(Monom* monoms[], int n);
 Monom* createMonom(int coefficient, int power){
 	Monom* myMonom = (Monom*)malloc(sizeof(Monom));
 	if(coefficient == 0) return NULL;
-	myMonom -> coefficient = Coefficient;
-	myMonom -> power = Power;
+	myMonom -> coefficient = coefficient;
+	myMonom -> power = power;
 	return myMonom;
 
 }
 void printMonom(Monom* monom){
-	printf("printMonom\n");
+	if (monom != NULL){
+		int currentCoefficient = monom -> coefficient;
+		int currentPower = monom -> power;
+
+		if(currentPower > 1){
+			if(currentCoefficient == 1){
+				printf("x^%d", currentPower);
+			}else if(currentCoefficient == -1){
+				printf("-x^%d", currentPower);
+			}else{
+				printf("%dx^%d", currentCoefficient, currentPower);
+			}
+
+		}else if(currentPower == 1){
+			if(currentCoefficient == 1){
+				printf("x");
+			}else if(currentCoefficient == -1){
+				printf("-x");
+			}else{
+				printf("%dx", currentCoefficient);
+			}
+		}else if(currentPower == 0){
+			printf("%d", currentCoefficient);
+
+		}else{
+			printf("%dx^%d", currentCoefficient, currentPower);
+
+		}
+	}
 }
 void addMonom(Polynom* polynom, int coefficient, int power){
-	if (!polynom || (coefficient == 0 || polynom -> numOfElements == SIZE)){
-		return;
-	}
-	else{
+	if (polynom && (coefficient != 0 && polynom -> numOfElements != SIZE)){
 		int currentElemNum = polynom -> numOfElements;
 		if(!currentElemNum){
 			currentElemNum = 0;
 		}
-		polynom -> monoms[currentElemNum] = createMonom(coefficient, power);
-		++polynom -> numOfElements;
+		int found = 0;
+		for(int i = 0; i < currentElemNum; i++){
+			if(power == polynom -> monoms[i] -> power){
+				polynom -> monoms[i] -> coefficient += coefficient;
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0){
+			polynom -> monoms[currentElemNum] = createMonom(coefficient, power);
+			++polynom -> numOfElements;
+		}
 	}
 }
 void printPolynom(Polynom polynom){
-	printf("printPolynom on %p\n", &polynom.monoms);
 	int numberOfElements = polynom.numOfElements;
+	insertionSort(polynom.monoms, numberOfElements);
 	for(int i = 0; i < numberOfElements; i++){
 		Monom *currentMonom = polynom.monoms[i];
 		int currentCoefficient = currentMonom -> coefficient;
 		int currentPower = currentMonom -> power;
-		printf("%dx^%d\t", currentCoefficient, currentPower);
+		if(i > 0 && (currentCoefficient > 0 && i != numberOfElements)){
+			printf("+");
+		}
+		printMonom(currentMonom);
 	}
 	printf("\n");
 
 }
 void deletePolynom(Polynom polynom){
-	printf("deletePolynom\n");
+	for(int i=0; i < polynom.numOfElements; i++){
+		free(polynom.monoms[i]);
+	}
+}
+
+void insertionSort(Monom* monoms[], int n)
+{
+	Monom* keyP;
+    int i, j;
+    for (i = 1; i < n; i++) 
+    {
+        keyP = monoms[i];
+        j = i - 1;
+ 
+        /* Move elements of arr[0..i-1], 
+           that are greater than key, 
+           to one position ahead of 
+           their current position */
+        while (j >= 0 && monoms[j] -> power < keyP -> power) 
+        {
+            monoms[j + 1] = monoms[j];
+            j = j - 1;
+        }
+        monoms[j + 1]= keyP;
+    }
 }
--- projects/exerciseThree/main.c
+++ projects/exerciseThree/main.c
@@ -8,21 +8,24 @@ int main(int argc, char const *argv[])
 	Polynom polynom1;
 	
 	polynom.numOfElements = 0; // Initialize the number of elements
-	printf("address of polynom is  %p\n", &polynom);
 
-	addMonom(&polynom, 3, 4); // 3x^4
-	addMonom(&polynom, 1, 4);
-	addMonom(&polynom, 4, 3); // 4x^3
-	addMonom(&polynom, 5, 2); // Add 5x^2, update existing monom to 8x^2
+	addMonom(&polynom, 3, 4);
+	addMonom(&polynom, -1, 2);
+	addMonom(&polynom, 4, 2);
+	addMonom(&polynom, 5, 4);
+	addMonom(&polynom, 4, 0);
+	addMonom(&polynom, -2, 0);
+	
+	printMonom(polynom.monoms[0]);
+	printf("\n");
 	printPolynom(polynom); // Print the polynom
 
 	polynom1.numOfElements = 0;
-	printf("address of polynom1 is  %p\n", &polynom1);
 
-	addMonom(&polynom1, 4, 5); // 3x^4
-	addMonom(&polynom1, 3, 4);
-	addMonom(&polynom1, 2, 3); // 4x^3
-	addMonom(&polynom1, 2, 2); // Add 5x^2, update existing monom to 8x^2
+	addMonom(&polynom1, 4, 2); // 3x^4
+	addMonom(&polynom1, 3, 3);
+	addMonom(&polynom1, 2, 2); // 4x^3
+	addMonom(&polynom1, -2, 3); // Add 5x^2, update existing monom to 8x^2
 	printPolynom(polynom1); // Print the polynom1
 
 	deletePolynom(polynom); // Clean up

44bce7825e1583f35d462dcb5b82dd4a81ab1e90
musa17-hawa <musa17-hawa@github.com>
2024-03-16 14:07:41 -0700

    ex4 done

--- projects/exerciseFour/Ex4.c
+++ projects/exerciseFour/Ex4.c
@@ -11,6 +11,8 @@
 * ---------------------------------------------------------------------------
 * 05/03/2024        Musa Abu Alhawa     Created.
 * 05/03/2024        Musa Abu Alhawa     Commit ID "a8d8fbc".
+* 16/03/2024        Musa Abu Alhawa     Reading file and setting up Data struct
+* 16/03/2024        Musa Abu Alhawa     Setting up filename.txt input as argv[], printing row index with all max elements 
 *
 /******************************************************************/
 
@@ -24,107 +26,133 @@ struct Data {
     int * lines_len;
 };
 
-// this is for dynamic string length, expands allocated string len value
-char *inputString(FILE* fp, size_t size){
-//The size is extended by the input with the value of the provisional
-    char *str;
-    int ch;
-    size_t len = 0;
-    str = realloc(NULL, sizeof(*str)*size);//size is start size
-    if(!str)return str;
-    while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
-        str[len++]=ch;
-        if(len==size){
-            str = realloc(str, sizeof(*str)*(size+=16));
-            if(!str)return str;
-        }
-    }
-    int i=0;
-    int result = 0;
-    while(str[i]!='\0') {
-        result +=str[i];
-        i++;
-    }
-    printf("%d", result);
-    str[len++]='\0';
-    return realloc(str, sizeof(*str)*len);
-}
-
-
 int get_num_of_lines(FILE* fp){
-    rewind(fp);
     int num;
     fscanf(fp, "%d", &num);
     return num;
 }
 
-void get_lines_len(FILE* fp, int *ptr){
-    rewind(fp);
-    int local_num_of_lines = get_num_of_lines(fp);
-    
-    printf("\n%d is the local numOfLines\n\n", local_num_of_lines);
+int scan_next_int(FILE *fp){
     int num;
-    char ch = getc(fp);
-    int counter = 0;
-    while(ch != EOF){
-        if (ch == '\n'){
-            counter++;
-            if (counter > local_num_of_lines) break;
-            fscanf(fp, "%d", &num);
-            printf("%d\n", num);
-            printf("pointer value is%d\n", *ptr);
-            ch = getc(fp);
-            
-        }
-        else{
-            ch = getc(fp);
-            continue;
-        }
+    fscanf(fp, "%d", &num);
+    return num;
+}
+
+void read_data(FILE *fp, int *arr, int n){
+    for(int i =0; i < n; i++){
+        arr[i] = scan_next_int(fp);
     }
 }
 
+int findMaxInArr(int *arr, int n){
+    int max = arr[0];
+    for(int i =0; i < n; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+
+void sortData(int **tmpData, int * lines_len, int num_of_lines, int maxCol){
+    int originalData[num_of_lines][findMaxInArr(lines_len,num_of_lines)];
+    for(int line = 0; line < num_of_lines; line++){
+        for(int elem = 0; elem < lines_len[line]; elem++){
+            originalData[line][elem] = tmpData[line][elem];
+        }
+    }
+    int index = -1;
+    int k;
+    int maxArr[findMaxInArr(lines_len, num_of_lines)]; 
+    for(k = 0; k < maxCol; k++){
+        int i, j;
+        int rows = num_of_lines;
+        for(i = 0; i < rows - 1; i++){
+            for(j = 0; j < rows - i  - 1; j++){
+                if (tmpData[j][k] < tmpData [j+1][k]){
+                    int * tmpArr = malloc(sizeof(int)*(lines_len[j]));
+                    tmpArr = tmpData[j] ;
+                    tmpData[j] = tmpData[j+1];
+                    tmpData[j+1] = tmpArr;
+                }
+            }
+        }
+        maxArr[k] = tmpData[0][k];
+    }
+    for(int line = 0; line < num_of_lines; line++){
+        if(index != -1){
+            break;
+        }
+        for(int elem = 0; elem < lines_len[line]; elem++){
+            if (originalData[line][elem] == maxArr[elem]){
+                if(elem == lines_len[line] - 1){
+                    index = line;
+                }
+            }else{
+                break;
+            }
+        }
+
+    }
+    printf("%d\n", index);
+
+}
+
+
+int findMaxInCol(int **tmpData, int n){
+    int max = tmpData[0][0];
+    for(int i = 0; i < n; i++){
+        if (tmpData[i][0] > max){
+            max = tmpData[i][0];
+        }
+    }
+    return max;
+}
 int main(int argc, char const *argv[])
 {
     struct Data myData; 
     // file pointer
     FILE* fp;
     // creating file using fopen() access mode "w"
-    fp = fopen("filename.txt", "r");
-    inputString(fp, 10);
+    fp = fopen(argv[1], "r");
     // checking if file was loaded
+
+
     if(fp == NULL){
         printf("File does not exist\texiting ...\n");
         exit(0);
     }
     else{
-        printf("file opened.\n");
 
-        int my_num_of_lines = get_num_of_lines(fp);
-        printf("Num of elem using function is: %d\n", my_num_of_lines);
-        myData.num_of_lines = my_num_of_lines;
+        myData.num_of_lines = get_num_of_lines(fp);
+        myData.lines_len = malloc(myData.num_of_lines*sizeof(int));
+        myData.the_data = malloc(sizeof(int *)*myData.num_of_lines);
 
-        myData.lines_len = (int*)malloc(myData.num_of_lines*sizeof(int));
-        get_lines_len(fp, myData.lines_len);
-        printf("output is: %d\n", *myData.lines_len);
-        // int num;
-        // fscanf(fp, "%d", &num);
-        // char ch = getc(fp);
-        // int counter = 0;
-        // while(ch != EOF){
-        //     myData.num_of_lines = counter == 0 ? num : myData.num_of_lines;
-        //     printf("%d\n",num);
-        //     fscanf(fp, "%d", &num);
-        //     counter++;
-        //     ch = getc(fp);
-        // }
-        // if(feof(fp)){
-        //     printf("End of File Reached.\n");
-        // }else{
-        //     printf("Something went wrong.\n");
-        // }
+        // Main Code goes below.
+
+        for(int line = 0; line < myData.num_of_lines; line++){
+            int numOfElements = scan_next_int(fp);
+            myData.lines_len[line] = numOfElements;
+            int *array = malloc(sizeof(int)*numOfElements);;
+            read_data(fp, array, numOfElements);
+            myData.the_data[line] = array;
+        }
+
+
+        // sorting starts
+        int **tmpData = myData.the_data;
+
+
+        sortData(tmpData, myData.lines_len, myData.num_of_lines, findMaxInArr(myData.lines_len, myData.num_of_lines));
+        int maxinCol = findMaxInCol(tmpData, myData.num_of_lines);
+
         fclose(fp);
-        printf("file closed.\n");
     }
     free(myData.lines_len);
+    for(int i = 0; i < myData.num_of_lines; i++){
+        free(myData.the_data[i]);
+    }
+    free(myData.the_data);
 	return 0;
 }
